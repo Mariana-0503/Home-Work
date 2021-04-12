12600c9edc72ddc779019ed4a670fb737ae2982f
Mariana0503 <nefertiti0503@gmail.com>
2021-04-12 15:22:33 +0300

    зміни

--- HW_23/code_10.js
+++ HW_23/code_10.js
@@ -3,3 +3,4 @@ let date = prompt('Enter the date - day-month-year ');
     let oldDate = new Date(date[2], date[1]-1, date[0]); //месяцы начинаются с 0
     oldDate.setDate(oldDate.getDate()+1);
     alert(oldDate);
+    
