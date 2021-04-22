29842a7180ef9fe2f07f7e06aa88b7ee3d0c9619
Mariana0503 <nefertiti0503@gmail.com>
2021-04-22 20:00:35 +0300

    перероблене 1 завдання

--- HW_26/code_1.js
+++ HW_26/code_1.js
@@ -1,28 +1,30 @@
-//Создать объект, описывающий автомобиль (производитель, модель, 
+//Создать объект, описывающий автомобиль (производитель, модель,
 //год выпуска, средняя скорость), и следующие функции для работы с этим объектом:
 
 //   + Функция для вывода на экран информации об автомобиле;
 
-//Функция для подсчета необходимого времени для преодоления 
-//переданного расстояния со средней скоростью. Учтите, что через 
+//Функция для подсчета необходимого времени для преодоления
+//переданного расстояния со средней скоростью. Учтите, что через
 //каждые 4 часа дороги водителю необходимо делать перерыв на 1 час.
 
-
-let car ={
-    manufacturer: "KIA",
-    model: "Sportage",
-    year: 2021,
-    speed: 140,
-    info() {
-        console.log(car)
-    },
-    travelTime (a) {
-     let  timeSimple = a/this.speed;
-     let timeBreake = timeSimple%4;
-     let  timeInRoad = timeSimple+timeBreake;
-        console.log(timeInRoad)
+let car = {
+  manufacturer: "KIA",
+  model: "Sportage",
+  year: 2021,
+  speed: 140,
+  info() {
+    console.log(car);
+  },
+  travelTime(a) {
+    let timeSimple = a / this.speed; //10
+    let timeBreake = 0;
+    timeBreake = Math.floor(timeSimple / 4);
+    if (timeSimple % 4 === 0) {
+      timeBreake -= 1;
     }
-
+    let timeInRoad = timeSimple + timeBreake;
+    console.log(timeInRoad);
+  },
 };
-car.info()
-car.travelTime(1400)
+car.info();
+car.travelTime(1400);
